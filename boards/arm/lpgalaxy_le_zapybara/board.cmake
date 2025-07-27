85d3777afc72b60dcab522a2445fc97bd542a9f3
ken <ken@123.org>
2025-07-28 05:31:37 +0800

    dkljfkewjfioemcxcn fjdskljfkwe

--- boards/arm/lpgalaxy_le_zapybara/board.cmake
+++ boards/arm/lpgalaxy_le_zapybara/board.cmake
@@ -1,6 +1,5 @@
-# Copyright (c) 2025 The ZMK Contributors
 # SPDX-License-Identifier: MIT
 
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
